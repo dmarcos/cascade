351be78f843bef9440d3c15c96f215fa8aca2350
Diego Marcos <diego.marcos@gmail.com>
2012-04-29 22:03:25 -0700

    adds matrix inverse / transpose / scale / determinant / multiplyScalar operations with corresponding tests

--- src/matrix4.js
+++ src/matrix4.js
@@ -17,7 +17,7 @@
         typeof m30 != 'number' || typeof m31 != 'number' ||
         typeof m32 != 'number' || typeof m33 != 'number') {
 
-      this.identity();
+      return this.identity();
 
     } else {
 
@@ -34,30 +34,23 @@
   // PROTOTYPE //
   ///////////////
 
+  Cascade.Matrix4.prototype.toString = function() {
+    return "| " + this.m00 + " " + this.m01 + " " + this.m02 + " " + this.m03 + " |\n" +
+           "| " + this.m10 + " " + this.m11 + " " + this.m12 + " " + this.m13 + " |\n" +
+           "| " + this.m20 + " " + this.m21 + " " + this.m22 + " " + this.m23 + " |\n" +
+           "| " + this.m30 + " " + this.m31 + " " + this.m32 + " " + this.m33 + " |\n";
+
+  };
+
   Cascade.Matrix4.prototype.set = function(m00, m01, m02, m03, 
                                            m10, m11, m12, m13, 
                                            m20, m21, m22, m23, 
                                            m30, m31, m32, m33) {
 
-    this.m00 = m00;
-    this.m01 = m01;
-    this.m02 = m02;
-    this.m03 = m03;
-
-    this.m10 = m10;
-    this.m11 = m11;
-    this.m12 = m12;
-    this.m13 = m13;
-
-    this.m20 = m20;
-    this.m21 = m21;
-    this.m22 = m22;
-    this.m23 = m23;
-
-    this.m30 = m30;
-    this.m31 = m31;
-    this.m32 = m32;
-    this.m33 = m33;
+    this.m00 = m00; this.m01 = m01; this.m02 = m02; this.m03 = m03;
+    this.m10 = m10; this.m11 = m11; this.m12 = m12; this.m13 = m13;
+    this.m20 = m20; this.m21 = m21; this.m22 = m22; this.m23 = m23;
+    this.m30 = m30; this.m31 = m31; this.m32 = m32; this.m33 = m33;
 
     return this;
 
@@ -65,12 +58,10 @@
 
   Cascade.Matrix4.prototype.identity = function() {
 
-    this.set(1,0,0,0,
-             0,1,0,0,
-             0,0,1,0,
-             0,0,0,1);
-
-    return this;
+    return new Cascade.Matrix4(1,0,0,0,
+                               0,1,0,0,
+                               0,0,1,0,
+                               0,0,0,1);
 
   };
 
@@ -104,7 +95,86 @@
              m.m30, m.m31, m.m32, m.m33);
 
     return this;
-    
+
   };
+
+  Cascade.Matrix4.prototype.transpose = function() {
+    return new Cascade.Matrix4(this.m00, this.m10, this.m20, this.m30, 
+                               this.m01, this.m11, this.m21, this.m31, 
+                               this.m02, this.m12, this.m22, this.m32, 
+                               this.m03, this.m13, this.m23, this.m33);
+  };
+
+  Cascade.Matrix4.prototype.determinant = function() {
+
+    // Found in http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm    
+    var m00 = this.m00, m01 = this.m01, m02 = this.m02, m03 = this.m03,
+        m10 = this.m10, m11 = this.m11, m12 = this.m12, m13 = this.m13,
+        m20 = this.m20, m21 = this.m21, m22 = this.m22, m23 = this.m23,
+        m30 = this.m30, m31 = this.m31, m32 = this.m32, m33 = this.m33; 
+
+    return  (m03*m12*m21*m30 - m02*m13*m21*m30 - m03*m11*m22*m30 + m01*m13*m22*m30+
+             m02*m11*m23*m30 - m01*m12*m23*m30 - m03*m12*m20*m31 + m02*m13*m20*m31+
+             m03*m10*m22*m31 - m00*m13*m22*m31 - m02*m10*m23*m31 + m00*m12*m23*m31+
+             m03*m11*m20*m32 - m01*m13*m20*m32 - m03*m10*m21*m32 + m00*m13*m21*m32+
+             m01*m10*m23*m32 - m00*m11*m23*m32 - m02*m11*m20*m33 + m01*m12*m20*m33+
+             m02*m10*m21*m33 - m00*m12*m21*m33 - m01*m10*m22*m33 + m00*m11*m22*m33);
+
+  };
+
+  Cascade.Matrix4.prototype.scale = function(x, y, z) {
+
+    return new Cascade.Matrix4(x*this.m00, y*this.m01, z*this.m02, this.m03, 
+                               x*this.m10, y*this.m11, z*this.m12, this.m13, 
+                               x*this.m20, y*this.m21, z*this.m22, this.m23, 
+                               x*this.m30, y*this.m31, z*this.m32, this.m33);
+
+  };
+
+  Cascade.Matrix4.prototype.multiplyScalar = function(scalar) {
+
+    return new Cascade.Matrix4(scalar*this.m00, scalar*this.m01, scalar*this.m02, scalar*this.m03, 
+                               scalar*this.m10, scalar*this.m11, scalar*this.m12, scalar*this.m13, 
+                               scalar*this.m20, scalar*this.m21, scalar*this.m22, scalar*this.m23, 
+                               scalar*this.m30, scalar*this.m31, scalar*this.m32, scalar*this.m33);
+
+  };
+
+  Cascade.Matrix4.prototype.inverse = function() {
+
+    // Found in http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm
+    var m00 = this.m00, m01 = this.m01, m02 = this.m02, m03 = this.m03,
+        m10 = this.m10, m11 = this.m11, m12 = this.m12, m13 = this.m13,
+        m20 = this.m20, m21 = this.m21, m22 = this.m22, m23 = this.m23,
+        m30 = this.m30, m31 = this.m31, m32 = this.m32, m33 = this.m33; 
+
+    var i00 = m12*m23*m31 - m13*m22*m31 + m13*m21*m32 - m11*m23*m32 - m12*m21*m33 + m11*m22*m33;
+    var i01 = m03*m22*m31 - m02*m23*m31 - m03*m21*m32 + m01*m23*m32 + m02*m21*m33 - m01*m22*m33;
+    var i02 = m02*m13*m31 - m03*m12*m31 + m03*m11*m32 - m01*m13*m32 - m02*m11*m33 + m01*m12*m33;
+    var i03 = m03*m12*m21 - m02*m13*m21 - m03*m11*m22 + m01*m13*m22 + m02*m11*m23 - m01*m12*m23;
+    var i10 = m13*m22*m30 - m12*m23*m30 - m13*m20*m32 + m10*m23*m32 + m12*m20*m33 - m10*m22*m33;
+    var i11 = m02*m23*m30 - m03*m22*m30 + m03*m20*m32 - m00*m23*m32 - m02*m20*m33 + m00*m22*m33;
+    var i12 = m03*m12*m30 - m02*m13*m30 - m03*m10*m32 + m00*m13*m32 + m02*m10*m33 - m00*m12*m33;
+    var i13 = m02*m13*m20 - m03*m12*m20 + m03*m10*m22 - m00*m13*m22 - m02*m10*m23 + m00*m12*m23;
+    var i20 = m11*m23*m30 - m13*m21*m30 + m13*m20*m31 - m10*m23*m31 - m11*m20*m33 + m10*m21*m33;
+    var i21 = m03*m21*m30 - m01*m23*m30 - m03*m20*m31 + m00*m23*m31 + m01*m20*m33 - m00*m21*m33;
+    var i22 = m01*m13*m30 - m03*m11*m30 + m03*m10*m31 - m00*m13*m31 - m01*m10*m33 + m00*m11*m33;
+    var i23 = m03*m11*m20 - m01*m13*m20 - m03*m10*m21 + m00*m13*m21 + m01*m10*m23 - m00*m11*m23;
+    var i30 = m12*m21*m30 - m11*m22*m30 - m12*m20*m31 + m10*m22*m31 + m11*m20*m32 - m10*m21*m32;
+    var i31 = m01*m22*m30 - m02*m21*m30 + m02*m20*m31 - m00*m22*m31 - m01*m20*m32 + m00*m21*m32;
+    var i32 = m02*m11*m30 - m01*m12*m30 - m02*m10*m31 + m00*m12*m31 + m01*m10*m32 - m00*m11*m32;
+    var i33 = m01*m12*m20 - m02*m11*m20 + m02*m10*m21 - m00*m12*m21 - m01*m10*m22 + m00*m11*m22;
+    
+    var adjugate = new Cascade.Matrix4(i00, i01, i02, i03, 
+                                       i10, i11, i12, i13, 
+                                       i20, i21, i22, i23, 
+                                       i30, i31, i32, i33);
+
+    return adjugate.multiplyScalar(1 / this.determinant());
+
+  };
+
+  // multiply, translate, scale, 
+  // rotate, rotateAxisAngle, skewX, skewY, toString
 
 })();
--- test/matrix4.js
+++ test/matrix4.js
@@ -34,9 +34,76 @@ describe('Matrix4', function() {
                                       5, 0, 1, 0,
                                       1,-7, 4, 5);
     var matrixB = new Cascade.Matrix4();
-    matrixB.copy(matrixA);
 
+    matrixB.copy(matrixA);
     expect(matrixA.compare(matrixB)).to.equal(true);
+
+  });
+
+  it('should transpose an arbitrary matrix', function() {
+    
+    var matrix = new Cascade.Matrix4(9, 1,-3, 1,
+                                     1, 0, 0,-9,
+                                     5, 0, 1, 0,
+                                     1,-7, 4, 5);
+
+    var expected = new Cascade.Matrix4(9, 1, 5, 1,
+                                       1, 0, 0,-7,
+                                      -3, 0, 1, 4,
+                                       1,-9, 0, 5);
+
+    expect(matrix.transpose().compare(expected)).to.equal(true);
+
+  });
+
+  it('determinant should be 0', function() {
+
+    var matrix = new Cascade.Matrix4(9, 1,-3, 1,
+                                     1, 0, 0,-9,
+                                     5, 0, 1, 0,
+                                     1, 0, 0,-9);
+    
+    expect(matrix.determinant()).to.equal(0);
+
+  });
+
+  it('determinant should not be 0', function() {
+
+    var matrix = new Cascade.Matrix4(3, 0, 0, 0,
+                                     0, 1, 0, 0,
+                                     0, 0,-1, 0,
+                                     0, 0, 0, 2);
+    
+    expect(matrix.determinant()).to.not.equal(0);
+
+  });
+
+  it('checks the inverse operation on the identity matrix', function() {
+
+    var identity = new Cascade.Matrix4(1, 0, 0, 0,
+                                       0, 1, 0, 0,
+                                       0, 0, 1, 0,
+                                       0, 0, 0, 1);
+
+    var inverse = identity.inverse();
+    expect(inverse.compare(identity)).to.be.equal(true);
+
+  });
+
+  it('checks the inverse operation on an arbitrary matrix', function() {
+
+    var matrix = new Cascade.Matrix4(1, 0, 0, 1,
+                                     0, 2, 1, 2,
+                                     2, 1, 0, 1,
+                                     2, 0, 1, 4);
+
+    var inverseExpected = new Cascade.Matrix4(-2,-0.5, 1, 0.5,
+                                               1, 0.5, 0,-0.5,
+                                              -8,  -1, 2,   2,
+                                               3, 0.5,-1,-0.5);
+
+    var inverseResult = matrix.inverse();
+    expect(inverseResult.compare(inverseExpected)).to.be.equal(true);
 
   });
 
